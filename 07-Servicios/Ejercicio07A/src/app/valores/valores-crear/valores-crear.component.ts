95d56f6ae75db307c8464fd2035fafb66c65f4e0
Jesus Almansa <jetalfer@gmail.com>
2025-09-22 09:15:55 +0200

    Componente para crear instancias y que los cambios sean persistentes

--- 07-Servicios/Ejercicio07A/src/app/valores/valores-crear/valores-crear.component.ts
+++ 07-Servicios/Ejercicio07A/src/app/valores/valores-crear/valores-crear.component.ts
@@ -1,52 +1,42 @@
 import { NgClass } from '@angular/common';
 import { Component, inject, signal } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { DatosDirectosService } from '../datos-directos.service';
 import { Valor } from '../modelo/valor';
+import { FormularioComponent } from '../../formulario/formulario.component';
 
 @Component({
   selector: 'app-valores-crear',
   standalone: true,
-  imports: [ReactiveFormsModule, NgClass],
+  imports: [ReactiveFormsModule, FormularioComponent],
   templateUrl: './valores-crear.component.html',
   styleUrls: ['../estilos/estilos.css','./valores-crear.component.css']
 })
 export class ValoresCrearComponent {
   private datos=inject(DatosDirectosService);
 
-  formulario = new FormGroup({
-    codigo: new FormControl('', [Validators.required, Validators.minLength(2)]),
-    nombre: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]),
-    valor: new FormControl('', [Validators.required, Validators.min(1), Validators.max(100)])
-});
 
-bien=signal(false);
-mal=signal(false);
-esperando=signal(false);
+  bienFormulario=signal(false);
+  malFormulario=signal(false);
+  esperandoFormulario=signal(false);
 
-  crear() {
-    if (this.formulario.invalid) return;
+  valor = signal<Valor | null>(null);
 
-    //Ha validado, por lo que estoy seguro de que los valores son correctos. 
-    //Pero el compilador no lo sabe...
-    let valor=new Valor(Number(this.formulario.value.codigo), 
-                        this.formulario.value.nombre || '', 
-                        Number(this.formulario.value.valor));
+  crear(valor:Valor | null) {
+    if (valor==null) return;
 
-    this.bien.set(false);
-    this.mal.set(false);
-    this.esperando.set(true);
+    this.bienFormulario.set(false);
+    this.malFormulario.set(false);
+    this.esperandoFormulario.set(true);
 
     this.datos.crear(valor).subscribe({
-      next:estado=>this.bien.set(true),
+      next:estado=>this.bienFormulario.set(true),
       error:estado=>{
-        this.mal.set(true);
-        this.esperando.set(false);
+        this.malFormulario.set(true);
+        this.esperandoFormulario.set(false);
       },
-      complete:()=>{this.esperando.set(false)}
+      complete:()=>{this.esperandoFormulario.set(false)}
     });
 
   }
-
-
 }
