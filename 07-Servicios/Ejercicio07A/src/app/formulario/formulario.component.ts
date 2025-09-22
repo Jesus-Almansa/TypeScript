8211714593d81d6a7412bf5f499a9bacae9fcddb
Jesus Almansa <jetalfer@gmail.com>
2025-09-22 09:15:10 +0200

    Componente formulario generico para poder usarlo en diferentes componentes como crear y modificar

---
+++ 07-Servicios/Ejercicio07A/src/app/formulario/formulario.component.ts
@@ -0,0 +1,65 @@
+import { Component, EventEmitter, inject, Input, OnChanges, Output, signal, SimpleChanges } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { DatosDirectosService } from '../valores/datos-directos.service';
+import { NgClass } from '@angular/common';
+import { Valor } from '../valores/modelo/valor';
+
+@Component({
+  selector: 'app-formulario',
+  standalone: true,
+  imports: [ReactiveFormsModule, NgClass],
+  templateUrl: './formulario.component.html',
+  styleUrls: ['./formulario.component.css', '../../app/valores/estilos/estilos.css']
+})
+export class FormularioComponent{
+  
+    formulario = new FormGroup({
+      codigo: new FormControl('', [Validators.required, Validators.minLength(2)]),
+      nombre: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]),
+      valor: new FormControl('', [Validators.required, Validators.min(1), Validators.max(100)])
+    });
+
+    @Input() bienFormulario=signal(false);
+    @Input() malFormulario=signal(false);
+    @Input() esperandoFormulario=signal(false);
+    @Input() valor = signal<Valor | null>(null);
+    @Input() mensajeBotonFormulario = '';
+
+    @Output() valorChange = new EventEmitter<Valor | null>();
+
+  crear() {
+      if (this.formulario.invalid) return;
+
+      //Ha validado, por lo que estoy seguro de que los valores son correctos. 
+      //Pero el compilador no lo sabe...
+      let valor=new Valor(Number(this.formulario.value.codigo), 
+                          this.formulario.value.nombre || '', 
+                          Number(this.formulario.value.valor));
+      this.valorChange.emit(valor);
+  }
+
+  modificar() {
+    if (this.formulario.invalid) return;
+    if (this.valor() == null) return;
+    //Ha validado, por lo que estoy seguro de que los valores son correctos.
+    //Pero el compilador no lo sabe...
+    let valor=new Valor(this.valor()?.id ?? 0, 
+                        this.formulario.value.nombre || '', 
+                        Number(this.formulario.value.valor));
+    this.valorChange.emit(valor);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
+    //Add '${implements OnChanges}' to the class.
+    if (changes['valor'] && this.valor() != null) {
+      this.formulario.setValue({
+        codigo: this.valor()?.id?.toString() ?? '',
+        nombre: this.valor()?.nombre?.toString() ?? '',
+        valor: this.valor()?.precio?.toString() ?? ''
+      });
+    } else {
+      this.formulario.reset();
+    }
+  }
+}
